15f51ae9136239978494ef33701b3269866e5206
LoveTechPi <loveleensharmaks@gmail.com>
2025-12-22 18:43:29 +0530

    Add files via upload

---
+++ Cable_RP2040/CircuitPython/Example2_Notepad/code.py
@@ -0,0 +1,51 @@
+import board
+import time
+import usb_hid
+import neopixel
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keycode import Keycode
+from keyboard_layout_win_uk import KeyboardLayout
+
+# Define the data pin and the number of pixels
+pixel_pin = board.GP25  # Example: using GP22 on a Pico
+num_pixels = 1         # Example: 30 NeoPixels in your strip
+
+# Initialize the NeoPixel object 
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.5, auto_write=False)
+
+# Set a single pixel to red
+pixels[0] = (255, 0, 0)
+pixels.show()
+time.sleep(0.5)
+
+# Fill the entire strip with blue
+pixels.fill((0, 0, 255))
+pixels.show()
+time.sleep(0.5)
+
+pixels.fill((0, 0, 255)) # Blue
+pixels.show()
+time.sleep(0.5)
+
+# Create a keyboard object
+keyboard = Keyboard(usb_hid.devices)
+keyboard_layout = KeyboardLayout(keyboard)
+
+# Give the computer some time to recognize the device
+time.sleep(2)
+
+keyboard.send(Keycode.WINDOWS, Keycode.R)
+time.sleep(0.5)
+keyboard_layout.write('notepad')
+keyboard.send(Keycode.ENTER)
+time.sleep(1)
+
+keyboard_layout.write("I am Dummy Text")
+time.sleep(3)
+
+# Press Enter to submit the password
+keyboard.press(Keycode.ENTER)
+keyboard.release_all()
+
+
+
